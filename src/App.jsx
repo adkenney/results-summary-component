da050ef6828709b11abbe5ddfc17c6bd212c63b9
Arkell Kenney <kell@Arkells-MacBook-Air.local>
2023-03-01 11:41:27 -0500

    add semantic html

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
 import ResultOverview from './components/ResultOverview';
 import ResultSummary from './components/ResultSummary';
-import data from './assets/data/data.js';
+import data from '../public/data/data.json';
 
 function App() {
   return (
-    <div className="flex justify-center items-center h-screen bg-paleblue overflow-auto">
-      <div className="flex flex-col w-[48em] mx-auto md:grid md:grid-cols-2 h-100 md:h-[32em] md:rounded-[2em] bg-white">
+    <main className="flex justify-center items-center h-screen bg-paleblue overflow-auto">
+      <section className="flex flex-col w-[48em] mx-auto md:grid md:grid-cols-2 h-100 md:h-[32em] md:rounded-[2em] bg-white">
         <ResultOverview data={data} />
         <ResultSummary data={data} />
-      </div>
-    </div>
+      </section>
+    </main>
   );
 }
 
